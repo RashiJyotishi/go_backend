71c0b0f90f3150a0082c7b269e71d25dc40b02cd
RashiJyotishi <jyotishirashi1610@gmail.com>
2026-02-05 21:44:51 +0530

    refactor: Update user model definition

--- models/user.go
+++ models/user.go
@@ -1,21 +1,20 @@
 package models
-import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type SignupRequest struct {
-    Username        string `json:"username"`
-    Email           string `json:"email"`
-    Password        string `json:"password"`
-    ConfirmPassword string `json:"confirm_password"`
-}
-
-type User struct {
-    ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-    Username string             `json:"username" bson:"username"`
-    Email    string             `json:"email" bson:"email"`
-    Password string             `json:"password" bson:"password"`
+	Username        string `json:"username"`
+	Email           string `json:"email"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirm_password"`
 }
 
 type LoginRequest struct {
-    Email    string `json:"email"`
-    Password string `json:"password"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type User struct {
+	ID       int
+	Username string
+	Email    string
+	Password string
 }
